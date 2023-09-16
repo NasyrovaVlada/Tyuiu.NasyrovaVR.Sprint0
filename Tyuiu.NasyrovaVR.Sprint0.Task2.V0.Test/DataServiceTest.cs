3649d2b74232105de7b84f90cdccc3e2bb83e6f8
NasyrovaVlada <Tyuiu.NasyrovaVR@outlook.com>
2023-09-16 15:16:27 +0500

    Task2

---
+++ Tyuiu.NasyrovaVR.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.NasyrovaVR.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.NasyrovaVR.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageVaild()
+        {
+            var name = "Влада";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Влада", res);
+        }
+    }
+}
